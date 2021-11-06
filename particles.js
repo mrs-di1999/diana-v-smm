e7141b3cce3eef6d5e6f53d6eeaf741514be1057
noidea91 <noidea91@gmail.com>
2021-11-06 13:42:24 +0300

    3

--- particles.js
+++ particles.js
@@ -91,13 +91,13 @@ var colorArray = [
 
 function drawCircles(){
     for (let i = 0; i < 1; i++) {
-        let radius = Math.floor(Math.random() * 10) + 4;
+        let radius = Math.floor(Math.random() * 10) + 1;
         let vx = (Math.random() * 1.5) - 1;
         let vy = (Math.random() * 1.5) - 1;
         let spawnFrame = frame;
         let rgb = colorArray[Math.floor(Math.random() * colorArray.length)];
-        let life = 77;
-        circleArray.push(new Circle(mouse.x, mouse.y, radius, vx, vy, rgb, 1, spawnFrame, life));
+        let life = Math.floor(Math.random() * 100) + 30;
+        circleArray.push(new Circle(mouse.x, mouse.y, radius, vx, vy, rgb, Math.random(), spawnFrame, life));
 
     }
 }
